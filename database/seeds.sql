3b670af3215c430e14c21d4b481d3a6f05b49be4
applegrubber <jscottatwood@gmail.com>
2019-04-27 21:30:42 -0600

    adding the working eat the burger mysql homework with no css

---
+++ database/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ('Cheese Burger', TRUE);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Mushroom Swiss', FALSE);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Crown Burger', FALSE);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Pulled Pork', TRUE);
